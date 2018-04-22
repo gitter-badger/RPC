6b1c50050236b14dc78213fea8bde8e7ccf5b30f
Purpzie <madf042@gmail.com>
2018-04-22 14:59:41 -0500

    Silly Discord

--- start.js
+++ start.js
@@ -3,9 +3,50 @@ const flag = require('flags');
 
 // get flag crap
 flag.defineString('use', 'main');
+// flag.defineString('type', 'start'); // will be used later for things like restarting on save edit
 flag.parse();
-const save = require(`./saves/${flag.get('use')}.js`);
-const rpc = DiscordRPC(save.app);
+
+// Get path for the save.
+const savePath = `./saves/${flag.get('use')}.js`;
+
+// Change the save into an RPC-usable object.
+function makeStatus(save) {
+  return {
+    instance: true,
+    partyID: (save.partySize && save.partyMax) ? 'discord-multiple-rpc' : undefined,
+    details: save.topText,
+    state: save.bottomText,
+    largeImageKey: save.largeImageName,
+    largeImageText: save.largeImageHover,
+    smallImageKey: save.smallImageName,
+    smallImageText: save.smallImageHover,
+    partySize: save.partySize,
+    partyMax: save.partyMax,
+    joinSecret: (save.joinButton) ? 'discord-multiple-rpc-joinButton' : undefined,
+    spectateSecret: (save.spectateButton) ? 'discord-multiple-rpc-spectateButton' : undefined,
+    startTimestamp: save.startTime,
+    endTimestamp: save.endTime
+  };
+}
+
+// Create the RPC.
+const { app } = require(savePath);
+var rpc = DiscordRPC(app);
+
+// Actually create the presence.
+rpc.on('connected', () => {
+  console.log(`Connected, using save: ${flag.get('use')}.`);
+
+  let save = require(savePath);
+  rpc.updatePresence(makeStatus(save));
+
+  // Make sure to remind Discord to keep it going.
+  setInterval(
+    () => {
+      rpc.updatePresence(makeStatus(save));
+    }, 3600000 // 1 hour
+  );
+});
 
 /*
   Nobody will actually be able to join or
@@ -20,30 +61,6 @@ rpc.on('spectate', secret => {
 });
 rpc.on('joinRequest', user => {
   user.reply(user, 'NO');
-});
-
-// Change the save into an RPC-usable object.
-var status = {
-  instance: true,
-  partyID: (save.partySize && save.partyMax) ? 'discord-multiple-rpc' : undefined,
-  details: save.topText,
-  state: save.bottomText,
-  largeImageKey: save.largeImageName,
-  largeImageText: save.largeImageHover,
-  smallImageKey: save.smallImageName,
-  smallImageText: save.smallImageHover,
-  partySize: save.partySize,
-  partyMax: save.partyMax,
-  joinSecret: (save.joinButton) ? 'discord-multiple-rpc-joinButton' : undefined,
-  spectateSecret: (save.spectateButton) ? 'discord-multiple-rpc-spectateButton' : undefined,
-  startTimestamp: save.startTime,
-  endTimestamp: save.endTime
-};
-
-// Actually create the presence.
-rpc.on('connected', () => {
-  console.log(`Connected, using save: ${flag.get('use')}.`);
-  rpc.updatePresence(status);
 });
 
 // Extra safety measures.
